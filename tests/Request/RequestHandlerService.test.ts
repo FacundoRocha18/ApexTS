ec86ce0f7dce72d107341d16eaf088b386cd1dba
Facundo Rocha <facundo.rocha@outlook.com>
2024-10-23 19:17:06 -0300

    test(RequestHandlerService.test): Adds new test cases.
    New test cases for RequestHandlerService:
    
    - Should execute middlewares and resolve the route.
    
    - Should set req.query correctly.
    
    - Should resolve route with empty pathname as "/".

--- tests/Request/RequestHandlerService.test.ts
+++ tests/Request/RequestHandlerService.test.ts
@@ -1,49 +1,71 @@
-import { IRequestHandlerService } from "../../src/Interfaces/RequestHandlerService.interface";
 import { RequestHandlerService } from "../../src/Request/RequestHandlerService";
-import { IRouter } from '../../src/Interfaces/Router.interface';
-import { IMiddlewareManager } from '../../src/Interfaces/MiddlewareManager.interface';
-import { Middleware, Request, Response } from '../../src/Types/main';
-
-jest.mock("../../src/Routing/Router.ts");
-jest.mock("../../src/Middlewares/MiddlewareManager.ts");
+import { IMiddlewareManager } from "../../src/Interfaces/MiddlewareManager.interface";
+import { IRouter } from "../../src/Interfaces/Router.interface";
+import { Request, Response } from "../../src/Types/main";
 
 describe("RequestHandlerService", () => {
-	let mockRouter: Partial<IRouter>;
-	let mockMiddlewareManager: IMiddlewareManager;
-	let requestHandlerService: IRequestHandlerService;
+  let requestHandlerService: RequestHandlerService;
+  let middlewareManager: jest.Mocked<IMiddlewareManager>;
+  let router: jest.Mocked<IRouter>;
+  let req: Request;
+  let res: Response;
 
-	beforeEach(() => {
-		mockRouter = {
-			resolveRoute: jest.fn(),
-		};
+  beforeEach(() => {
+    middlewareManager = {
+      executeMiddlewares: jest.fn(),
+    } as Partial<IMiddlewareManager> as jest.Mocked<IMiddlewareManager>;
 
-		mockMiddlewareManager = {
-			use: jest.fn((middleware: Middleware) => middleware),
-			executeMiddlewares: jest.fn((req: Request, res: Response, next: () => void) => next()),
-		};
+    router = {
+      resolveRoute: jest.fn(),
+    } as Partial<IRouter> as jest.Mocked<IRouter>;
 
-		requestHandlerService = new RequestHandlerService(
-			mockMiddlewareManager as IMiddlewareManager,
-			mockRouter as IRouter
-		);
-	});
+    requestHandlerService = new RequestHandlerService(
+      middlewareManager,
+      router,
+    );
 
-	it("should handle a new request with handleRequest method", () => {
-		const req = {
-			url: "/",
-			method: "GET",
-		} as Request;
-		const res = {} as Response;
-		const { url, method } = req;
-		
-		const mockMiddleware: Middleware = jest.fn((req, res, next) => next());
-		const next: () => void = () => {};
+    req = {
+      method: "GET",
+      url: "/test?param=value",
+    } as Partial<Request> as Request;
 
-		mockMiddlewareManager.use(mockMiddleware)
+    res = {
+      end: jest.fn(),
+    } as Partial<Response> as Response;
+  });
 
-		requestHandlerService.handleRequest(req, res);
+  it("should execute middlewares and resolve the route", () => {
+    requestHandlerService.handleRequest(req, res);
 
-		expect(mockMiddlewareManager.executeMiddlewares).toHaveBeenCalledWith(req, res, next);
-		expect(mockRouter.resolveRoute).toHaveBeenCalledWith(req, res, url, method);
-	});
+    expect(middlewareManager.executeMiddlewares).toHaveBeenCalledWith(
+      req,
+      res,
+      expect.any(Function),
+    );
+
+    const next = middlewareManager.executeMiddlewares.mock.calls[0][2];
+    if (next) {
+      next();
+    }
+
+    expect(router.resolveRoute).toHaveBeenCalledWith(req, res, "/test", "GET");
+  });
+
+  it("should set req.query correctly", () => {
+    requestHandlerService.handleRequest(req, res);
+
+    expect(req.query).toEqual({ param: "value" });
+  });
+
+  it('should resolve route with empty pathname as "/"', () => {
+    req.url = "";
+    requestHandlerService.handleRequest(req, res);
+
+    const next = middlewareManager.executeMiddlewares.mock.calls[0][2];
+    if (next) {
+      next();
+    }
+
+    expect(router.resolveRoute).toHaveBeenCalledWith(req, res, "/", "GET");
+  });
 });
