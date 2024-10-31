413a51e9304ef809d9b6b3af032f0e66cd610606
Facundo Rocha <67356139+FacundoRocha18@users.noreply.github.com>
2024-10-30 23:59:13 -0300

    feat(factory): enhance FrameworkFactory with detailed JSDoc comments for better documentation

--- lib/factory/framework-factory.ts
+++ lib/factory/framework-factory.ts
@@ -1,57 +1,75 @@
 import {
-  IMiddlewareManager,
-  jsonResponseMiddleware,
-  MiddlewareManager,
-  TMiddlewareFunction,
+	IMiddlewareManager,
+	jsonResponseMiddleware,
+	MiddlewareManager,
+	TMiddlewareFunction,
 } from "../middlewares";
 import {
-  IRouteProcessorService,
-  IRouter,
-  RouteProcessorService,
-  Router,
+	IRouteProcessorService,
+	IRouter,
+	RouteProcessorService,
+	Router,
 } from "../router";
 import { IRequestHandler, RequestHandler } from "../http";
 import { IParserService, ParserService } from "../parser";
 import { Framework, IFramework } from "../application";
 
+
+/**
+ * FrameworkFactory creates a new instance of a Framework with the provided services.
+ * @param parserService - The parser service to use, it uses a default ParserService if none is provided
+ * @param router - The router to use, it uses a default Router if none is provided
+ * @param routeProcessorService - The route processor service to use, it uses a default RouteProcessorService if none is provided
+ * @param middlewareManager - The middleware manager to use, it uses a default MiddlewareManager if none is provided
+ * @param requestHandler - The request handler to use, it uses a default RequestHandler if none is provided
+ */
 export class FrameworkFactory {
 	private parserService: IParserService;
 	private router: IRouter;
 	private routeProcessorService: IRouteProcessorService;
 	private middlewareManager: IMiddlewareManager;
 	private requestHandler: IRequestHandler;
-  
+
 	constructor(
 		parserService?: IParserService,
 		router?: IRouter,
 		routeProcessorService?: IRouteProcessorService,
 		middlewareManager?: IMiddlewareManager,
 		requestHandler?: IRequestHandler,
-  ) {
+	) {
 		this.parserService = parserService || new ParserService();
 		this.router = router || new Router(this.parserService);
 		this.routeProcessorService = routeProcessorService || new RouteProcessorService(this.router, this.parserService);
-    this.middlewareManager =
-      middlewareManager || new MiddlewareManager(this.routeProcessorService);
-    this.requestHandler =
-      requestHandler || new RequestHandler(this.middlewareManager, this.router);
-  }
+		this.middlewareManager =
+			middlewareManager || new MiddlewareManager(this.routeProcessorService);
+		this.requestHandler =
+			requestHandler || new RequestHandler(this.middlewareManager, this.router);
+	}
 
-  public create(): IFramework {
-    const framework = new Framework(
-      this.router,
-      this.middlewareManager,
-      this.requestHandler,
-    );
+	/**
+	 * Creates a new instance of a Framework with the provided services.
+	 * @returns A new instance of a Framework
+	 */
+	public create(): IFramework {
+		const framework = new Framework(
+			this.router,
+			this.middlewareManager,
+			this.requestHandler,
+		);
 
-    this.middlewareManager.use(jsonResponseMiddleware);
-    return framework;
-  }
+		this.middlewareManager.use(jsonResponseMiddleware);
+		return framework;
+	}
 
-  public withCustomMiddleware(
-    middleware: TMiddlewareFunction,
-  ): FrameworkFactory {
-    this.middlewareManager.use(middleware);
-    return this;
-  }
+	/**
+	 * Adds a custom middleware to the middleware manager.
+	 * @param middleware - The middleware function to add
+	 * @returns The FrameworkFactory instance
+	 */
+	public withCustomMiddleware(
+		middleware: TMiddlewareFunction,
+	): FrameworkFactory {
+		this.middlewareManager.use(middleware);
+		return this;
+	}
 }
