9ca8be5013b1f859449e08eafb1fafcea85077d3
Facundo Rocha <facundo.rocha@outlook.com>
2024-11-10 20:26:01 -0300

    fix: Update response format in users controller to use res.json for better consistency

--- examples/controllers/users-controller.ts
+++ examples/controllers/users-controller.ts
@@ -4,5 +4,5 @@ export const usersController = (req: IHttpRequest, res: IHttpResponse) => {
   const { id, name } = req.queryParams as { id: string, name: string };
 
   res.statusCode = 200;
-  res.end(`User ID: ${id}, Query Params: ${JSON.stringify(name)}`);
+  res.json(`User ID: ${id}, User name: ${name}`);
 }
