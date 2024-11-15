477ecca50a0d8bbf4a932db753653f175b18cc2c
Facundo Rocha <facundo.rocha@outlook.com>
2024-11-15 19:27:19 -0300

    Refactor: Update IRouter interface method parameters for improved clarity and consistency

--- lib/router/router.interface.ts
+++ lib/router/router.interface.ts
@@ -2,14 +2,13 @@ import { HttpRequest, HttpResponse, Controller } from "../types";
 import { HttpMethods } from "../http";
 
 export interface IRouter {
-  use(method: HttpMethods, path: string, controller: Controller): void;
-  get(path: string, handler: Controller): void;
-  post(path: string, handler: Controller): void;
-  put(path: string, handler: Controller): void;
-  del(path: string, handler: Controller): void;
-  patch(path: string, handler: Controller): void;
+  use(httpMethod: HttpMethods, url: string, controller: Controller): void;
+  get(url: string, controller: Controller): void;
+  post(url: string, controller: Controller): void;
+  put(url: string, controller: Controller): void;
+  del(url: string, controller: Controller): void;
+  patch(url: string, controller: Controller): void;
 
-  resolveRoute(req: HttpRequest, res: HttpResponse, path: string, method: string): void;
-
-  processRoute(req: HttpRequest, res: HttpResponse, url: string, method: string): void;
+  resolveRoute(req: HttpRequest, res: HttpResponse, url: string, httpMethod: string): void;
+  processRoute(req: HttpRequest, res: HttpResponse, url: string, httpMethod: string): void;
 }
