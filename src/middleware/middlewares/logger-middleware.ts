ca461b00deb40b4c9d9c64cc4c3545526d10664c
Facundo Rocha <facundo.rocha@outlook.com>
2024-11-30 17:21:16 -0300

    Refactor: enhance loggerMiddleware with exception handling for improved error management

--- src/middleware/middlewares/logger-middleware.ts
+++ src/middleware/middlewares/logger-middleware.ts
@@ -1,7 +1,11 @@
-import { HttpRequest } from "../../http/request";
-import { HttpResponse } from "../../http/response";
+import { MiddlewareException } from '@exceptions';
+import { Middleware } from '@middleware';
 
-export const loggerMiddleware = (req: HttpRequest, res: HttpResponse, next: () => void): void | Promise<void> => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+export const loggerMiddleware: Middleware = (req, res, next): void | Promise<void> => {
+	try {
+		console.log(`${req.method} ${req.url}`);
+		next();
+	} catch (error) {
+		next(new MiddlewareException(error.message, 500, error.stack));
+	}
 };
