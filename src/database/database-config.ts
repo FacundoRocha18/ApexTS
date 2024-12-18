3a54121ebee7f86520b2ce63d20d6176049930a7
Facundo Rocha <facundo.rocha@outlook.com>
2024-12-17 21:50:39 -0300

    feat(database): update database configuration to use environment variables for connection settings

--- src/database/database-config.ts
+++ src/database/database-config.ts
@@ -3,13 +3,18 @@ import { Customer } from '../../examples/customers/customer';
 
 export class DatabaseConfig {
 	public static getDatabaseOptions(): DataSourceOptions {
+		console.log('DB_HOST', process.env.DB_HOST);
+		console.log('DB_PORT', process.env.DB_PORT);
+		console.log('DB_USERNAME', process.env.DB_USERNAME);
+		console.log('DB_PASSWORD', process.env.DB_PASSWORD);
+		console.log('DB_DATABASE', process.env.DB_DATABASE);
 		return {
 			type: "postgres",
-			host: "localhost",
-			port: parseInt(process.env.DB_PORT || '5432'),
-			username: "postgres",
-			password: "Fr1810Fr!",
-			database: "apexts-test",
+			host: process.env.DB_HOST as string,
+			port: parseInt(process.env.DB_PORT as string),
+			username: process.env.DB_USERNAME as string,
+			password: process.env.DB_PASSWORD as string,
+			database: process.env.DB_DATABASE as string,
 			synchronize: true,
 			logging: false,
 			entities: [
