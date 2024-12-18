b81528760423ef51cadc197ca11a0105d87071b9
Facundo Rocha <facundo.rocha@outlook.com>
2024-12-17 21:55:12 -0300

    chore(package): bump version to 3.0.1
    refactor(database): remove console logs for database connection settings

--- src/database/database-config.ts
+++ src/database/database-config.ts
@@ -3,11 +3,6 @@ import { Customer } from '../../examples/customers/customer';
 
 export class DatabaseConfig {
 	public static getDatabaseOptions(): DataSourceOptions {
-		console.log('DB_HOST', process.env.DB_HOST);
-		console.log('DB_PORT', process.env.DB_PORT);
-		console.log('DB_USERNAME', process.env.DB_USERNAME);
-		console.log('DB_PASSWORD', process.env.DB_PASSWORD);
-		console.log('DB_DATABASE', process.env.DB_DATABASE);
 		return {
 			type: "postgres",
 			host: process.env.DB_HOST as string,
