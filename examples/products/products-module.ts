c3bb87e758bbf90353e6eca169ae058b71f46b35
Facundo Rocha <facundo.rocha@outlook.com>
2024-12-28 13:54:24 -0300

    refactor(products): update import paths for consistency and clarity

--- examples/products/products-module.ts
+++ examples/products/products-module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@modules';
-import { Product } from 'examples/products/product';
-import { router as productsRouter } from 'examples/products/products-routes';
-import { ProductsService } from 'examples/products/products-service';
+import { Product } from './product';
+import { router as productsRouter } from './products-routes';
+import { ProductsService } from './products-service';
 
 export const ProductsModule: Module = {
 	entities: [Product],
